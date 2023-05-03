024a1d043bffeae99334f084778b2fd39a93aa18
dariamilk <daryachulpanova@gmail.com>
2023-05-03 22:37:55 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/homework5/Homework5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
